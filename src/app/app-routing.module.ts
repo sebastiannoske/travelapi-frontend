68a92d4f148965130372f266c343b9176cfffb95
kkoal <k.koal@student.htw-berlin.de>
2017-09-17 17:10:51 +0200

    add list views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,31 +5,42 @@ import { EventResolver } from './travels/event-resolver.service';
 
 import { PageNotFoundComponent } from './page-not-found.component';
 import { MainComponent } from './travels/main/main.component';
+import { TravelListComponent } from './travels/travel-list/travel-list.component';
+import { TravelNewComponent } from './travels/travel-new/travel-new.component';
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot([
-      {
-        path: ':id',
-        component: MainComponent,
-        resolve: { event: EventResolver },
-        children: [
-          {
-            path: 'offers',
-            component: PageNotFoundComponent,
-            data: { type: 'offer' }
-          },
-          {
-            path: 'requests',
-            component: PageNotFoundComponent,
-            data: { type: 'request' }
-          }
-        ]
-      },
-      { path: '**', component: PageNotFoundComponent }
-    ])
-  ],
-  providers: [EventResolver],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot([
+            {
+                path: ':id',
+                component: MainComponent,
+                resolve: { eventHead: EventResolver },
+                children: [
+                    {
+                        path: '',
+                        redirectTo: 'offers',
+                        pathMatch: 'full'
+                    },
+                    {
+                        path: 'offers',
+                        component: TravelListComponent,
+                        data: { type: 'offer' }
+                    },
+                    {
+                        path: 'requests',
+                        component: TravelListComponent,
+                        data: { type: 'request' }
+                    },
+                    {
+                        path: 'new',
+                        component: TravelNewComponent
+                    }
+                ]
+            },
+            { path: '**', component: PageNotFoundComponent }
+        ])
+    ],
+    providers: [EventResolver],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {}
---
+++ src/app/travels/travel-new/travel-new.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-travel-new',
+    templateUrl: './travel-new.component.html',
+    styleUrls: ['./travel-new.component.scss']
+})
+export class TravelNewComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
