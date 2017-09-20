8a32feefe0e9798372247a975f4651f5f1112c79
kkoal <k.koal@student.htw-berlin.de>
2017-09-20 11:51:48 +0200

    refactor event repository

--- src/app/travels/event-repository.service.ts
+++ src/app/travels/event-repository.service.ts
@@ -7,41 +7,29 @@ import { Event, EventHead, Travel, Destination } from './interfaces/_index';
 
 @Injectable()
 export class EventRepository {
-    private _event: Event;
-    private _;
+  private _event: Event;
 
-    constructor() {}
+  constructor() {}
 
-    public set event(event: Event) {
-        this._event = event;
-    }
+  public set event(event: Event) {
+    this._event = event;
+  }
 
-    public get eventHead(): EventHead {
-        return {
-            offers_total: this.offers.length,
-            requests_total: this.requests.length
-        };
-    }
-    public get requests(): Travel[] {
-        return this._event
-            .map(destination => destination.travel)
-            .reduce((travels, current) => {
-                return travels.concat(
-                    current.filter(travel => {
-                        return travel.request !== null;
-                    })
-                );
-            });
-    }
-    public get offers(): Travel[] {
-        return this._event
-            .map(destination => destination.travel)
-            .reduce((travels, current) => {
-                return travels.concat(
-                    current.filter(travel => {
-                        return travel.offer !== null;
-                    })
-                );
-            });
-    }
+  public get eventHead(): EventHead {
+    return {
+      offers_total: this.offers.length,
+      requests_total: this.requests.length
+    };
+  }
+  public get travels(): Travel[] {
+    return this._event
+      .map(destination => destination.travel)
+      .reduce((travels, current) => travels.concat(current));
+  }
+  public get requests(): Travel[] {
+    return this.travels.filter(travel => travel.request);
+  }
+  public get offers(): Travel[] {
+    return this.travels.filter(travel => travel.offer);
+  }
 }
