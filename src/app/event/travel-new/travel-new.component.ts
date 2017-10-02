a2d32731f8c57a48c296813c0e22068c5066c85c
kkoal <k.koal@student.htw-berlin.de>
2017-10-02 15:50:01 +0200

    add first form steps

--- src/app/event/travel-new/travel-new.component.ts
+++ src/app/event/travel-new/travel-new.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import {
+    FormGroup,
+    FormControl,
+    FormBuilder,
+    Validators,
+    AbstractControl
+} from '@angular/forms';
 
 import { TravelSubmission } from '../classes/_index';
 
@@ -11,17 +17,59 @@ import { TravelSubmission } from '../classes/_index';
 export class TravelNewComponent implements OnInit {
     travel: TravelSubmission = new TravelSubmission();
     travelForm: FormGroup;
-    constructor() {}
+    activeStep = 1;
+    constructor(private _fb: FormBuilder) {}
 
     ngOnInit(): void {
-        this.travelForm = new FormGroup({
-            firstName: new FormControl(),
-            lastName: new FormControl()
+        this.travelForm = this._fb.group({
+            steps: this._fb.array([
+                this._fb.group({
+                    travelType: [
+                        <'offer' | 'request'>'offer',
+                        [Validators.required]
+                    ]
+                }),
+                this._fb.group({
+                    userName: ['', [Validators.required]],
+                    userAddress: ['', [Validators.required]],
+                    userPostCode: ['', [Validators.required]],
+                    userCity: ['', [Validators.required]],
+                    userPhoneNumber: ['', [Validators.required]]
+                    // userEmail: this._fb.group(
+                    //     {
+                    //         email: [
+                    //             '',
+                    //             [Validators.required, Validators.email]
+                    //         ],
+                    //         confirmEmail: ['', Validators.required]
+                    //     },
+                    //     { validator: this.emailMatcher }
+                    // )
+                })
+            ])
         });
     }
 
     save(): void {
         console.log(this.travelForm);
         console.log('Saved: ' + JSON.stringify(this.travelForm.value));
+    }
+
+    validateText(formControl: FormControl): boolean {
+        return (formControl.touched || formControl.dirty) && !formControl.valid;
+    }
+
+    emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
+        const emailControl = c.get('email');
+        const confirmControl = c.get('confirmEmail');
+
+        if (emailControl.pristine || confirmControl.pristine) {
+            return null;
+        }
+
+        if (emailControl.value === confirmControl.value) {
+            return null;
+        }
+        return { match: true };
     }
 }
