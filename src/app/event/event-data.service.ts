ba0351bbf7c412d27b97c5f07a41854b1d3df4c0
basti <sebastian.noske@gmail.com>
2018-09-25 21:20:40 +0200

    fix

--- src/app/event/event-data.service.ts
+++ src/app/event/event-data.service.ts
@@ -14,7 +14,7 @@ import { TravelSubmission } from './classes/_index';
 
 @Injectable()
 export class EventDataService {
-    private _eventsUrl = 'https://api.lesscars.io/api'; // 'http://travel-api.test/api';
+    private _eventsUrl = 'http://travel-api.test/api'; // 'https://api.lesscars.io/api'; // ;
     private _headers: HttpHeaders;
 
     constructor(private _http: HttpClient) {
